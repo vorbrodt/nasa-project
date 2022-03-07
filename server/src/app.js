70c3b4faff25c0375e83a8b848ae0592af7ec593
Maximilian Vorbrodt <maximilian.vorbrodt@hotmail.com>
2022-03-07 20:22:54 +0100

    add automation for fullstack development and stream loading data on startup

--- server/src/app.js
+++ server/src/app.js
@@ -1,15 +1,27 @@
 const express = require("express");
+const cors = require("cors");
+const path = require("path");
 
-const {getAllPlanets} = require("./routes/planets/planets.controller");
+const { getAllPlanets } = require("./routes/planets/planets.controller");
 
 const planetsRouter = require("./routes/planets/planets.router");
 
-
-
 const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
+
 app.use(planetsRouter);
 
+// inital page load in client
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 module.exports = app;
--- server/src/server.js
+++ server/src/server.js
@@ -3,9 +3,18 @@ const app = require("./app");
 
 const PORT = process.env.PORT || 8000;
 
-// allows server isolation from express code
+// import function loading the data
+const { loadPlanetsData } = require("./models/planets.model");
+
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-  console.log(`Listeningen on port ${PORT}`);
-});
+async function startServer() {
+  // wait for data to load
+  await loadPlanetsData();
+
+  server.listen(PORT, () => {
+    console.log(`Listeningen on port ${PORT}`);
+  });
+}
+
+startServer();
