df5afef38841ec3cfaac3fe2d1b7e7ff2dd7a342
vorbrodt <maximilianvorbrodt>
2022-03-09 08:37:57 +0100

    initial Jest setup

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,31 @@
+describe("Test GET /launches", () => {
+  test("SHOULD REPSOND 200",() => {
+    const reponse = 200;
+    //assertion
+    expect(200).toBe(200);
+
+  });
+})
+
+
+describe("Test POST /launches", () => {
+  test("should repsond with 200", () => {
+
+  })
+
+  test("catch missing properties", () => {
+
+
+  })
+  test("catch invalid date", () => {
+
+    
+  })
+
+})
+
+
+
+describe("Test DELETE /launches", () => {})
+
+
